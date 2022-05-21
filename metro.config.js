bb163c4adb93bb43ebe4bb28b12a294a39bffc48
Moritz Möller <mm@mxs.de>
2022-05-21 13:05:02 +0200

    ..

---
+++ metro.config.js
@@ -0,0 +1,12 @@
+console.error('react-native-mo-fontlink: metro.config.js');
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
