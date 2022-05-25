5d6ec20c230fb74f134f06f5a5d2b8649b55b3c2
Moritz Möller <mm@mxs.de>
2022-05-25 11:57:00 +0200

    ..

--- react-native.config.js
+++ react-native.config.js
@@ -1,6 +1,6 @@
 // this is called via main podfile
 // via node ./node_modules/.bin/react-native config
-// requires an xcodeproj in this project.
+// requires an xcodeproj in this project as well as a podfile
 
 /** @type {import('@react-native-community/cli-types').Config} */
 module.exports = {
@@ -11,6 +11,7 @@ module.exports = {
         scriptPhases: [
           {
             name: '[react-native-mo-fontlink] Link fonts',
+            // must be a shell script
             // path: 'node ./lib/fontlink.js',
             path: './fontlink-ios.sh',
             execution_position: 'after_compile',
